7305284f972c10ca73e47754217b171a49a3d906
김경도 <49026505+rudeh567@users.noreply.github.com>
2021-10-29 16:46:50 +0900

    Initial Commit

---
+++ bridgeWeb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bridgeWeb
+//
+//  Created by yeoboya on 2021/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
