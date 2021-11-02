1bfcfa2b1f80b31dd58994c71e889d5274dcf0d4
김경도 <49026505+rudeh567@users.noreply.github.com>
2021-11-02 17:20:32 +0900

    naver login

--- bridgeWeb/SignUpViewController.swift
+++ bridgeWeb/SignUpViewController.swift
@@ -14,32 +14,9 @@ import NaverThirdPartyLogin
 
 class SignUpViewController: UIViewController {
     
-    var webView: WKWebView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "SignUp!"
-        
-        loadView()
-        
-        guard let url = URL(string: "http://babyhoney.kr/login") else { return }
-        let request = URLRequest(url: url)
-        webView.load(request)
-        
-        
     }
-    
-    override func loadView() {
-        let webConfigurations = WKWebViewConfiguration()
-        let userScript = WKUserScript(source: "$.callFromWeb", injectionTime: .atDocumentEnd, forMainFrameOnly: true)
-    
-        let contentController = WKUserContentController()
-        contentController.addUserScript(userScript)
-        webConfigurations.userContentController = contentController
-        webView = WKWebView(frame: .zero, configuration: webConfigurations)
-        view = webView
-    }
-    
-    
 }
--- bridgeWeb/ViewController.swift
+++ bridgeWeb/ViewController.swift
@@ -12,9 +12,12 @@ import Alamofire
 import KakaoSDKAuth
 import NaverThirdPartyLogin
 
-class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, NaverThirdPartyLoginConnectionDelegate {
     
     @IBOutlet weak var webView: WKWebView!
+    
+    var bridge: WKWebViewJavascriptBridge!
+    let loginInstance = NaverThirdPartyLoginConnection.getSharedInstance()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +26,28 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
         
         print("viewDidLoad Call")
         
+        loginInstance?.delegate = self
+        
         loadUrl()
+        bridgeSetting()
+    }
+    
+    func oauth20ConnectionDidFinishRequestACTokenWithAuthCode() {
+        print("Success login")
+        getInfo()
+    }
+    
+    func oauth20ConnectionDidFinishRequestACTokenWithRefreshToken() {
+        loginInstance?.accessToken
+    }
+    
+    func oauth20ConnectionDidFinishDeleteToken() {
+        print("log out")
+        self.loginInstance?.requestAccessTokenWithRefreshToken()
+    }
+    
+    func oauth20Connection(_ oauthConnection: NaverThirdPartyLoginConnection!, didFailWithError error: Error!) {
+        print("error = \(error.localizedDescription)")
     }
     
     func loadUrl() {
@@ -36,6 +60,47 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
         
         webView.uiDelegate = self
         webView.navigationDelegate = self
+    }
+    
+    func getInfo() {
+        guard let isValidAccessToken = loginInstance?.isValidAccessTokenExpireTimeNow() else { return }
+        
+        if !isValidAccessToken {
+            return
+        }
+        
+        guard let tokenType = loginInstance?.tokenType else { return }
+        guard let accessToken = loginInstance?.accessToken else { return }
+        
+        let urlStr = "https://openapi.naver.com/v1/nid/me"
+        let url = URL(string: urlStr)!
+        
+        let authorization = "\(tokenType) \(accessToken)"
+        
+        let req = AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: ["Authorization": authorization])
+        
+        req.responseJSON { response in
+            guard let result = response.value as? [String: Any] else { return }
+            guard let object = result["response"] as? [String: Any] else { return }
+            guard let name = object["name"] as? String else { return }
+            guard let email = object["email"] as? String else { return }
+            guard let id = object["id"] as? String else { return }
+            
+            print(email)
+        }
+    }
+    
+    func bridgeSetting() {
+        bridge = WKWebViewJavascriptBridge.bridge(forWebView: webView!)
+        bridge?.registerHandler(handlerName: "$.callFromWeb", handler: { data, responseCallback in
+            
+            if String(describing: data).contains("loginNaver") {
+                self.loginInstance?.requestThirdPartyLogin()
+            } else {
+                print("찾을수없음")
+                self.loginInstance?.requestDeleteToken()
+            }
+        })
     }
     
     public func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
